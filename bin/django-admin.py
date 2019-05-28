b3584370212b5a90500f8c2cd27279069f961fc3
guineitor <guineitor.traps236@gmail.com>
2019-05-28 13:13:37 -0300

    initial commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/guilherme/Documents/Projects/guiprojects/MBA/cloud-devops/mygene/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
